7878b41819ad6095006348640637515ef9ef97dc
chamoddinuranga <chamod3246@gmail.com>
2024-08-23 09:59:03 +0530

    http://192.168.8.105:3000 added

--- src/main/java/lk/snt/dyeBackend/util/WebConfig.java
+++ src/main/java/lk/snt/dyeBackend/util/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:3000", "http://192.168.8.104:3000","http://192.168.8.106:3000") // Add frontend origin
+                    .allowedOrigins("http://localhost:3000", "http://192.168.8.104:3000","http://192.168.8.106:3000","http://192.168.8.105:3000") // Add frontend origin
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);
